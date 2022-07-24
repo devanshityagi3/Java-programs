02f672d2f58fa5933685d8fb4380220052125117
Devanshi <62430031+devanshityagi3@users.noreply.github.com>
2022-07-24 10:57:34 +0530

    prints the reverse of a number

---
+++ ReverseOfANumber.java
@@ -0,0 +1,12 @@
+public class ReverseOfANumber {
+    public static void main(String[] args) {
+        int n=79304; 
+        int rem,rev=0;
+        while(n>0){
+            rem=n%10;
+            rev=rev*10+rem;
+            n=n/10;
+        }
+        System.out.println(rev);
+    }
+}
